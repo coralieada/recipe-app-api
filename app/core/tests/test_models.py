9aca7098e38b57ae3be4a7fc0b4b9f53b51cb395
Coralie Ada mintoo <corymint@MacBook-Air-de-Coralie.local>
2026-04-19 20:04:36 +0200

    added test for users

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,30 @@
+"""
+tests for models.
+"""
+from django.test import TestCase # noqa F401
+from django.contrib.auth import get_user_model # noqa F401
+
+class ModelTests(TestCase):
+    """Tests for models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized."""
+        sample_emails = [
+            ['test1@example.com', 'test1@example.com'],
+            ['Test2@EXAMPLE.COM', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.Com', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email=email, password='testpass123')
+            self.assertEqual(user.email, expected)
